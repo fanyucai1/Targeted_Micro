e44b43665ea57d23261cb58d09a625be16d9051b
fanyucai1 <fanyucai1@126.com>
2024-02-22 18:34:47 +0800

    no

--- script/core/reduce redundancy.py
+++ script/core/reduce redundancy.py
@@ -1,5 +1,15 @@
 import subprocess
+import argparse
+
 def cdhit(fasta,outdir,prefix):## CD-HIT-EST is used to cluster similar contigs to reduce redundancy.
     out = outdir + "/" + prefix
     cmd = "/software/cd-hit-v4.8.1-2019-0228/cd-hit-est -i %s -o %s.fasta -c 0.95 -n 5 -g 1 -aS 0.8 -T 0"%(fasta,out)
     subprocess.check_call(cmd,shell=True)
+
+if __name__=="__main__":
+    parser=argparse.ArgumentParser("CD-HIT-EST is used to cluster similar contigs to reduce redundancy")
+    parser.add_argument("-f","--fasta",help="fasta sequence",required=True)
+    parser.add_argument("-o", "--outdir", help="output directory", required=True)
+    parser.add_argument("-p", "--prefix", help="prefix of output", required=True)
+    args = parser.parse_args()
+    cdhit(args.fasta,args.outdir,args.prefix)
--- script/core/trim_primer.py
+++ script/core/trim_primer.py
@@ -1,14 +1,25 @@
 import subprocess
+import argparse
 
 def ivar(bam,bed,outdir,prefix):
     out=outdir+"/"+prefix
     cmd = "/software/Miniconda3/bin/ivar trim -e -i %s -b %s -p %s.trim_primer | tee %s.ivar.stdout" % (bam, bed, out, out)
-    print(cmd)
     subprocess.check_call(cmd, shell=True)
     cmd = "samtools sort %s.trim_primer.bam -o %s.trim_primer.sort.bam && rm -rf %s.trim_primer.bam" % (out, out,out)
-    print(cmd)
     subprocess.check_call(cmd, shell=True)
-    cmd = "/software/openlogic-openjdk-jre-8u392-b08-linux-x64/bin/java -jar /software//jvarkit/jvarkit.jar biostar84452 --samoutputformat BAM %s.soft.clipped.sort.bam |samtools sort -n >%s.trimmed.bam" % (
-    out, out)
-    print(cmd)
+    cmd = ("/software/openlogic-openjdk-jre-8u392-b08-linux-x64/bin/java -jar /software//jvarkit/jvarkit.jar biostar84452 "
+           "--samoutputformat BAM %s.trim_primer.sort.bam |samtools sort -n >%s.trimmed.bam && rm -rf %s.trim_primer.sort.bam") % (
+    out, out,out)
     subprocess.check_call(cmd, shell=True)
+    cmd = ("/software/samtools-1.19.2/samtools fastq -1 %s_no_primer.R1.fq -2 %s_no_primer.R2.fq -s %s.singleton.fastq %s.trimmed.bam &>%s.bam2fastq.stdout"
+           % (out, out, out, out, out))
+    subprocess.check_call(cmd, shell=True)
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser("Trim primers with ivar.")
+    parser.add_argument("-m","--bam", help="sort bam file", required=True)
+    parser.add_argument( "-b","--bed", help="reference fasta", required=True)
+    parser.add_argument("-o","--outdir", help="output directory", required=True)
+    parser.add_argument("-p","--prefix", help="prefix of output", required=True)
+    args = parser.parse_args()
+    ivar(args.bam, args.bed, args.outdir, args.prefix)
