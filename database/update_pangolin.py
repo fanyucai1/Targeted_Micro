f061dd33eadb7547be8acb1bc432bae4e523465e
fanyucai1 <fanyucai1@126.com>
2024-03-07 10:52:39 +0800

    no

--- database/update_pangolin.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-cmd="wget https://github.com/cov-lineages/pangolin/archive/refs/heads/master.zip && "
-cmd+="unzip master.zip && "
-cmd+="docker rmi -f covlineages/pangolin && docker build -t covlineages/pangolin pangolin-master/"
-subprocess.check_call(cmd,shell=True)
